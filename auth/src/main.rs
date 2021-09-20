d165b7554118b74cd32290555e6f0638e0b1b631
Marcus Lee <marcustutorial@hotmail.com>
2021-09-20 13:19:49 +0800

    Setup auth service

--- auth/src/main.rs
+++ auth/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+mod handlers;
+
+use crate::config::Config;
+use actix_web::{middleware::Logger, App, HttpServer};
+use color_eyre::Result;
+use handlers::app_config;
+use tracing::info;
+
+#[actix_rt::main]
+async fn main() -> Result<()> {
+    let config: Config = Config::from_env().expect("Server configuration");
+
+    info!("Starting server at http://{}:{}", config.host, config.port);
+    HttpServer::new(move || App::new().wrap(Logger::default()).configure(app_config))
+        .bind(format!("{}:{}", config.host, config.port))?
+        .run()
+        .await?;
+
+    Ok(())
 }
