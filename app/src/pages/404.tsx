052473ba898da67602373616c22794cd1b69c770
Marcus Lee <marcustutorial@hotmail.com>
2021-12-24 01:14:38 +0800

    deleted app and web folder

--- app/src/pages/404.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from "next";
-
-const Error404: NextPage = () => {
-  return (
-    <div className="h-100vh flex justify-center items-center">Error 404</div>
-  );
-};
-
-export default Error404;
--- app/unocss.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig, presetIcons, presetUno } from "unocss";
-import { presetTypography } from "unocss-preset-typography";
-
-export default defineConfig({
-  shortcuts: [],
-  presets: [presetUno(), presetIcons(), presetTypography()],
-});
